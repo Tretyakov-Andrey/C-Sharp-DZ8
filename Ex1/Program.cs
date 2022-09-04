03a9607bebcaf2632ea10869e8dcb0800ee2c91b
Andrey <this.is.ya8@gmail.com>
2022-09-04 12:16:29 +1000

    DZ8

---
+++ Ex1/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] Fill(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    int count = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return arr;
+}
+int[,] Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j],4}");
+        }
+        System.Console.WriteLine();
+    }
+    return arr;
+}
+
+int[,] Sort(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            int pos = j;
+            for (int k = j; k < arr.GetLength(1); k++)
+            {
+                if (arr[i, k] > arr[i, pos]) pos = k;
+            }
+            int t = arr[i, pos];
+            arr[i, pos] = arr[i, j];
+            arr[i, j] = t;
+        }
+    }
+    return arr;
+}
+
+Print(Sort(Fill(5,5)));
---
+++ Ex2/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] Fill(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    int count = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+int FindResult(int[,] arr)
+{
+    int[] countStrings = new int [arr.GetLength(0)];
+    int sumElements = 0;
+    int result = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sumElements = sumElements + arr[i,j];
+        }
+        System.Console.WriteLine($"{i+1} = {sumElements}");
+        countStrings[i] = sumElements;
+        sumElements = 0;
+        result = countStrings[0];
+        
+    }
+    System.Console.WriteLine();
+    for (int i = 1; i < arr.GetLength(0); i++)
+    {
+        if (result > countStrings[i]) 
+        {
+            result = countStrings[i];
+        }
+    }
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        if(result == countStrings[i]) 
+        System.Console.WriteLine($"RESULT = String: {i+1}");
+    }
+
+return result;
+}
+
+int[,] Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j],4}");
+        }
+        System.Console.WriteLine();
+    }
+    return arr;
+}
+
+FindResult(Print(Fill(5,5)));
